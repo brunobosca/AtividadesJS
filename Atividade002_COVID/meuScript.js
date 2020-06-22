0f6f24075bff2ebc21a7d9c958372a8f49706360
brunobosca <brunoboscaini1@gmail.com>
2020-06-22 16:40:55 -0300

    atividade002concluida

---
+++ Atividade002_COVID/meuScript.js
@@ -0,0 +1,9 @@
+let lavarMaos = window.prompt('Você está lavando as mãos?')
+let mask = window.prompt('Você está usando máscara?')
+
+if (lavarMaos == "sim" && mask == "sim") {
+alert('Parabéns" Você é uma pessoa previnida!')
+}
+else {
+    alert('Você está errado!')
+}
