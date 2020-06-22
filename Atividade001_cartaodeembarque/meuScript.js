f5fdb080581c72c7ceaa3fc4305e3e76da36a7ee
brunobosca <brunoboscaini1@gmail.com>
2020-06-21 22:22:18 -0300

    atividade01_cartaodeembarqueconcluida

---
+++ Atividade001_cartaodeembarque/meuScript.js
@@ -0,0 +1,4 @@
+let nome = window.prompt('Olá, para fazer o seu check-in digite o seu nome')
+let numerovoo = window.prompt('Digite o número do seu vôo')
+let fileira = window.prompt('Em qual fileira você quer sentar? A, B, C, D ou F?')
+let cartaoEmbarque = alert(`Parabéns ${nome}! Você realizou o seu check-in do vôo número ${numerovoo} sua fileira é a ${fileira}`)
