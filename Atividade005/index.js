cb885d5c70cddd98f9e3d9ca43080064d17ea420
brunobosca <brunoboscaini1@gmail.com>
2020-06-23 15:27:29 -0300

    atividade005concluida

--- Atividade005/index.js
+++ Atividade005/index.js
@@ -1,2 +1,4 @@
 let readlineSync = require('readline-sync')
-
+let contadorLetras = readlineSync.question('Digite qualquer coisa ')
+let total = console.log(`O total de carcateres é ${contadorLetras.length}`)
+ 
